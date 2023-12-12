0efc2dfdc6a2e70cbc883803000027c17dd654c5
Neil Murphy <barrymun@tcd.ie>
2023-12-12 23:16:18 +0000

    accepting a space-separated list of symbols with the -s flag (symbols)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
-// import { program } from "commander";
+import { Command } from "commander";
 import figlet from "figlet";
-import { fetchStocks } from "lib/request";
+
+// import { fetchStocks } from "lib/request";
+
+const program = new Command();
 
 console.log(figlet.textSync("Stock Ticker"));
 
-const symbols = ["AAPL", "GOOG"];
+program
+  .version("1.0.0")
+  .description("description")
+  .requiredOption("-s, --symbols <symbols...>", "Space-separated list of stock symbols")
+  // .requiredOption('-s, --symbols <symbols>', 'Comma-separated list of stock symbols')
+  .parse(process.argv);
 
-const stocks = await fetchStocks({ symbols });
-console.log({ stocks });
+const inputSymbols = program.opts().symbols;
+console.log(inputSymbols);
+
+// if using comma-separated list of symbols
+// const symbols: string[] = inputSymbols.split(/[, ]+/).filter(Boolean);
+// console.log({ symbols });
+
+// test
+// const symbols = ["AAPL", "GOOG"];
+// const stocks = await fetchStocks({ symbols });
+// console.log({ stocks });
