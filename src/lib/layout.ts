3169dc5757c059f1aa58c8a201c89557917c75d9
Neil Murphy <barrymun@tcd.ie>
2023-12-17 17:45:09 +0000

    improved formatting for the stocks (wip)

--- src/lib/layout.ts
+++ src/lib/layout.ts
@@ -1,23 +1,90 @@
+import blessed from "blessed";
 import figlet from "figlet";
 
-import { updateOutputBox } from "lib/screen";
+import { getState, setState } from "lib/state";
 import { formatStocks } from "utils/helpers";
 import { FetchStocksResponse, FormattedStock } from "utils/types";
 
 const title: string = figlet.textSync("Stock Ticker");
 const exitMessage: string = "Press Ctrl+C to exit...";
 
-export const refreshLayout = (stocks: FetchStocksResponse[] | null) => {
-  const formattedStocks: FormattedStock[] = formatStocks(stocks ?? []);
+let topOffset: number = 0;
 
-  updateOutputBox(
-    `${title}\n` +
-      formattedStocks
-        .map((stock) => {
-          return `${stock.symbol}: ${stock.trend} ${stock.latestPrice} ${stock.change} ${stock.changePercent}`;
-        })
-        .join("\n") +
-      "\n\n" +
-      `${exitMessage}`,
+// Function to create a table-like border around a stock entry
+const createStockBorder = (stock: FormattedStock) => {
+  const box = blessed.box({
+    width: "100%-4",
+    height: 5, // Adjust the height as needed
+    top: topOffset,
+    border: "line",
+    // content: `
+    //   Symbol: ${stock.symbol}
+    //   Trend: ${stock.trend}
+    //   Latest Price: ${stock.latestPrice}
+    //   Change: ${stock.change}
+    //   Change Percent: ${stock.changePercent}
+    // `,
+    content: `${stock.symbol}: ${stock.trend} ${stock.latestPrice} ${stock.change} ${stock.changePercent}`,
+    tags: true,
+    style: {
+      border: {
+        fg: "white",
+      },
+    },
+  });
+  topOffset += 6;
+  return box;
+};
+
+export const refreshLayout = (stocks: FetchStocksResponse[] | null) => {
+  const { screen } = getState();
+
+  // reset the top offset
+  topOffset = 0;
+
+  screen.append(
+    blessed.box({
+      width: "100%-4",
+      height: 20, // Adjust the height as needed
+      top: topOffset,
+      border: "line",
+      content: title,
+      tags: true,
+      style: {
+        border: {
+          fg: "white",
+        },
+      },
+    }),
   );
+
+  topOffset += 22;
+
+  const formattedStocks: FormattedStock[] = formatStocks(stocks ?? []);
+  formattedStocks.forEach((stock) => {
+    const stockBorder = createStockBorder(stock);
+    screen.append(stockBorder);
+  });
+
+  topOffset += 6;
+
+  screen.append(
+    blessed.box({
+      width: "100%-4",
+      height: 5, // Adjust the height as needed
+      top: topOffset,
+      border: "line",
+      content: exitMessage,
+      tags: true,
+      style: {
+        border: {
+          fg: "white",
+        },
+      },
+    }),
+  );
+
+  screen.render();
+
+  setState({ screen });
 };
