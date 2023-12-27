ff31e26250f947e5874fd9ff869a894a1288d60e
Neil Murphy <barrymun@tcd.ie>
2023-12-27 14:39:41 +0000

    adding TODO but not required for now

--- tests/request.test.ts
+++ tests/request.test.ts
@@ -4,7 +4,10 @@ import { fetchStocks } from "lib/request";
 
 describe("Fetch stocks", () => {
   it("should fetch stocks", async () => {
+    // TODO: need to handle case when useSavedDataMode is false
+    // should be set before executing fetchStocks()
     const stocks = await fetchStocks();
+    // then should be unset after executing fetchStocks()
     expect(stocks).not.toBe(null);
   });
 });
